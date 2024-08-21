5f659b48e4d67ae2f7e4b75e8cf9a2b0c3c52d18
dingxiaotong <dingxiaotong11@yahoo.com>
2024-08-21 10:29:11 +0800

    update cube position

--- SandboxVK/src/Layers/VkRHIRenderGraphLayer.h
+++ SandboxVK/src/Layers/VkRHIRenderGraphLayer.h
@@ -28,12 +28,12 @@ public:
 
 		m_CubeObj->GetComponent<BlackPearl::Transform>()->SetScale({ 0.2,0.2,0.2 });
 		m_SphereObj->GetComponent<BlackPearl::Transform>()->SetScale({ 0.5,0.5,0.5 });
-		m_CubeObj->SetPosition({ 0.0,0.0,0.0 });
-		m_SphereObj->SetPosition({ 0.0,0.0,0.0 });
+		m_CubeObj->SetPosition({ 0.0,0.0,-2.0 });
+		m_SphereObj->SetPosition({ -0.4,0.0,-2.0 });
 
 
 		m_MainCamera->SetMoveSpeed(5.0f);
-		//m_Scene->AddObject(m_SphereObj);
+		m_Scene->AddObject(m_SphereObj);
 		m_Scene->AddObject(m_CubeObj);
 
 
