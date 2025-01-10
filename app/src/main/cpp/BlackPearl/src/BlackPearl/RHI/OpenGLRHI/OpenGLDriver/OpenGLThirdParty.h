fb53a552585ffb0fc5bc1a972af28c95b8b80aa1
DXT00 <dingxiaotong11@yahoo.com>
2025-01-10 17:52:16 +0800

    add opengl platform rhi

---
+++ app/src/main/cpp/BlackPearl/src/BlackPearl/RHI/OpenGLRHI/OpenGLDriver/OpenGLThirdParty.h
@@ -0,0 +1,11 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+//#include COMPILED_PLATFORM_HEADER(OpenGLThirdParty.h)
+
+#ifdef GE_PLATFORM_WINDOWS
+#include "glad/glad.h"
+#else
+//TODO:: Andriod gles lib
+#endif
